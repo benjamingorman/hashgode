0d213d33430907bcc8032a449dc1414d0665feb8
Benjamin Gorman <8076bgorman@gmail.com>
2017-02-25 04:43:32 +0000

    In which there is no lacking of bin packing

--- hashgode.go
+++ hashgode.go
@@ -25,7 +25,9 @@ type Problem struct {
 }
 
 type Solution struct {
-	assignments []CacheAssignment
+	assignments   []CacheAssignment
+	vcPairs       []VideoCachePair
+    videoRequests [][]int // maps videoID to list of requests for that video
 }
 
 type RequestBatch struct {
@@ -40,6 +42,7 @@ type Endpoint struct {
 	datacenterLatency int
 	cacheLatencies    map[int]int     // maps cache server IDs to latencies
 	preferredCaches   []IDLatencyPair // ordered list of cache ids based on latency (lowest first)
+    requests          []int // list of requests on this endpoint
 }
 
 type CacheServer struct {
@@ -70,9 +73,13 @@ type IDLatencyPair struct {
 
 // A pairing of a video with a cache server along with the saving that incurs
 type VideoCachePair struct {
-	videoID int
-	cacheID int
-	saving  int
+	videoID          int
+	cacheID          int
+    affectedRequests []int
+	saving           int
+    uses             int // how many requests actually use this cache?
+    assigned         bool // whether it's in use or not
+    ignoreSet        map[int]bool // list of request IDs to ignore (they aren't helped by the pair)
 }
 
 func initVideos(p *Problem) {
@@ -289,11 +296,150 @@ func setPreferredCaches(p *Problem) {
 	}
 }
 
+// Sets the requests field of every Endpoint
+func setEndpointRequests(p *Problem) {
+    fmt.Println("Set endpoint requests called")
+    for reqID, req := range p.requestBatches {
+        ep := &p.endpoints[req.endpointID]
+        ep.requests = append(ep.requests, reqID)
+    }
+
+    /*
+    for epID, ep := range p.endpoints {
+        fmt.Printf("Endpoint %v has %v requests\n", epID, len(ep.requests))
+    }
+    */
+}
+
+// Set the videoRequests field of the solution
+func setVideoRequests(p *Problem, sol *Solution) {
+    sol.videoRequests = make([][]int, p.numVideos)
+
+    for reqID, req := range p.requestBatches {
+        sol.videoRequests[req.videoID] = append(sol.videoRequests[req.videoID], reqID)
+    }
+
+    for vid := 0; vid < p.numVideos; vid++ {
+        numReqs := len(sol.videoRequests[vid])
+        if numReqs > 0 {
+            fmt.Printf("Video %v has %v requests\n", vid, numReqs)
+        }
+    }
+}
+
 func initSolutionAssignments(p *Problem, sol *Solution) {
 	sol.assignments = make([]CacheAssignment, p.numCaches, p.numCaches)
 	for i := 0; i < p.numCaches; i++ {
 		sol.assignments[i] = CacheAssignment{cacheID: i, usedCapacity: 0, videos: make(map[int]bool)}
 	}
+}
+
+func initSolutionVCPairs(p *Problem, sol *Solution) {
+    // Should be called after setEndpointRequests
+    fmt.Println("Initializing video-cache pairs")
+    maxRequests := 0
+    minRequests := 9999999
+    totalNumReqs := 0
+
+    // Maps videoID -> (cacheID -> *VideoCachePair)
+    var usedPairs map[int]map[int]*VideoCachePair = make(map[int]map[int]*VideoCachePair)
+
+    for vid := 0; vid < p.numVideos; vid++ {
+        var vidRequests []int
+        for reqID, req := range p.requestBatches {
+            //fmt.Printf("Req %v %+v\n", reqID, req)
+            if req.videoID == vid {
+                vidRequests = append(vidRequests, reqID)
+            }
+        }
+
+        numReqs := len(vidRequests)
+        totalNumReqs += numReqs
+        //fmt.Printf("Vid %v has %v requests\n", vid, numReqs)
+
+        if numReqs < minRequests {
+            minRequests = numReqs
+        } else if numReqs > maxRequests {
+            maxRequests = numReqs
+        }
+
+        for _, reqID := range vidRequests {
+            req := p.requestBatches[reqID]
+            ep := p.endpoints[req.endpointID]
+            for _, idLatPair := range ep.preferredCaches {
+                cID := idLatPair.id
+
+                // Check if we have a pair for this video/cache already
+                seen := false
+                if usedPairs[vid] == nil {
+                    usedPairs[vid] = make(map[int]*VideoCachePair)
+                } else if usedPairs[vid][cID] != nil {
+                    seen = true
+                    pair := usedPairs[vid][cID]
+                    pair.affectedRequests = append(pair.affectedRequests, reqID)
+                }
+
+                if !seen {
+                    // Make a new pair
+                    sol.vcPairs = append(sol.vcPairs, VideoCachePair{videoID: vid, cacheID: cID})
+                    currentPair := &sol.vcPairs[len(sol.vcPairs)-1] // the newly added pair
+                    currentPair.affectedRequests = append(currentPair.affectedRequests, reqID)
+                    //currentPair.ignoreSet = make(map[int]bool) // SET THIS IF ACTUALLY NEEDED
+                    usedPairs[vid][cID] = currentPair
+                }
+            }
+        }
+    }
+
+    fmt.Printf("minRequests %v maxRequests %v numPairs %v totalNumReqs %v\n",
+                minRequests, maxRequests, len(sol.vcPairs), totalNumReqs)
+}
+
+// Calls setVCPairSaving for every pair in the solution
+func setVCPairSavings(p *Problem, sol *Solution) {
+    fmt.Println("Setting savings...")
+    for i := 0; i < len(sol.vcPairs); i++ {
+        pair := &sol.vcPairs[i]
+        if !pair.assigned {
+            setVCPairSaving(p, sol, &sol.vcPairs[i])
+        }
+    }
+}
+
+// Calculates the potential saving if this pair is used (assuming that every request actually uses this cache)
+// Pays respect to the ignoreSet of the pair (ignores those requests)
+func setVCPairSaving(p *Problem, sol *Solution, pair *VideoCachePair) {
+    //fmt.Printf("Setting saving for pair %+v\n", pair)
+    cache := p.caches[pair.cacheID]
+
+    totalBasicTime := 0
+    totalCachedTime := 0
+    for _, reqID := range pair.affectedRequests {
+        req := p.requestBatches[reqID]
+        //fmt.Printf("    Affects request %v\n", req)
+        if pair.ignoreSet[reqID] {
+            //fmt.Printf("    Ignoring this request\n")
+            continue
+        }
+
+        ep := p.endpoints[req.endpointID]
+        basicTime := req.amount * ep.datacenterLatency
+        totalBasicTime += basicTime
+
+        cachedTime := req.amount * cache.endpointLatencies[req.endpointID]
+        totalCachedTime += cachedTime
+    }
+
+    pair.saving = totalBasicTime - totalCachedTime
+    //fmt.Printf("Pair (vid %v, cache %v) saves %+v\n", pair.videoID, pair.cacheID, pair.saving)
+}
+
+func sortSolutionVCPairs(p *Problem, sol *Solution) {
+    // Sorts all the pairs by the saving
+    fmt.Println("Sorting pairs...")
+    sort.SliceStable(sol.vcPairs, func(i int, j int) bool {
+        return sol.vcPairs[i].saving > sol.vcPairs[j].saving
+    })
 }
 
 func writeSolutionFile(filePath string, sol *Solution) {
@@ -349,15 +495,202 @@ func bestCacheLatency(p *Problem, ep *Endpoint) int {
 	return ep.datacenterLatency
 }
 
-func solveWithRequestOrder(p *Problem) (sol Solution) {
+func getCacheUsedByRequest(p *Problem, sol *Solution, req *RequestBatch) int {
+    ep := p.endpoints[req.endpointID]
+
+    for _, idLatPair := range ep.preferredCaches {
+        ass := sol.assignments[idLatPair.id]
+        if ass.videos[req.videoID] {
+            return idLatPair.id
+        }
+    }
+
+    return -1
+}
+
+func computeSolutionScore(p *Problem, sol *Solution) int {
+    score := 0
+    totalRequestAmounts := 0
+
+    for _, req := range p.requestBatches {
+        ep := p.endpoints[req.endpointID]
+        totalRequestAmounts += req.amount
+
+        reqCacheID := getCacheUsedByRequest(p, sol, &req)
+        reqLatency := ep.datacenterLatency
+
+        // It has a cache
+        if reqCacheID != -1 {
+            reqLatency = ep.cacheLatencies[reqCacheID]
+        }
+
+        savedTime := (ep.datacenterLatency - reqLatency) * req.amount
+        //fmt.Printf("Req %+v uses cache %v saved time %v\n", req, reqCacheID, savedTime)
+        score += savedTime
+    }
+
+    return int(1000.0 * float64(score) / float64(totalRequestAmounts))
+}
+
+func printSolution(sol *Solution) {
+    fmt.Println("Solution is:")
+    /*
+    for _, ass := range sol.assignments {
+        fmt.Printf("%+v\n", ass)
+    }
+    */
+}
+
+func doAssignments(p *Problem, sol *Solution) {
+    fmt.Println("Doing assignments")
+    for cID := 0; cID < p.numCaches; cID++ {
+        ass := sol.assignments[cID]
+        if ass.usedCapacity == p.cacheCapacity {
+            // No point trying cause it's already full
+            //fmt.Println("Cache %v is already full so skipping")
+            continue
+        }
+        freeSpace := p.cacheCapacity - ass.usedCapacity
+
+        // Find all pairs for this cache that aren't already assigned
+        // Some may have been unassigned during waste detection phase
+        var pairsForCache []*VideoCachePair
+
+        for i:=0; i < len(sol.vcPairs); i++ {
+            pair := &sol.vcPairs[i]
+
+            if pair.cacheID == cID && !pair.assigned {
+                pairsForCache = append(pairsForCache, pair)
+            }
+        }
+
+        // Parameters for bin packing problem
+        var weights []int
+        var values []int
+        var n int = len(pairsForCache)-1
+        var W int = freeSpace
+        /*
+        fmt.Printf("Computing knapsack solution for cache %v (%v items) (%v space)\n",
+                    cID, len(pairsForCache), W)
+                    */
+
+        for _, pair := range pairsForCache {
+            weights = append(weights, p.videoSizes[pair.videoID])
+            values = append(values, pair.saving)
+        }
+
+        var resultDynamic [][]int
+        resultDynamic = knapsackDynamic(weights, values, n, W)
+        //var finalValue int
+        var result []int
+        _, result = interpretKnapsackSolution(values, weights, resultDynamic, W, n)
+
+        //fmt.Printf("Solution found! %v pairs, finalValue %v\n", len(result), finalValue)
+        for _, pairIndex := range result {
+            pair := pairsForCache[pairIndex]
+            // Use the pair
+            sol.assignments[pair.cacheID].videos[pair.videoID] = true
+            pair.assigned = true
+        }
+
+        /*
+        if cID == 0 {
+            fmt.Println("Debugging bin packing")
+            fmt.Printf("Pairs %v\n", pairsForCache)
+            fmt.Printf("Weights %v\n", weights)
+            fmt.Printf("Values %v\n", values)
+            fmt.Printf("Result %v\n", result)
+        }
+        */
+    }
+}
+
+// Works out which pairs in the solution are not actually being used by requests
+func calculateWaste(p *Problem, sol *Solution) {
+    // Calculate wasteful assignments (not actually used by request)
+    fmt.Println("Calculating waste")
+    usefulPairs := 0
+    wastefulPairs := 0
+    var wastefulSet []int // list of indexes of the pairs that are wasteful
+    for i:=0; i < len(sol.vcPairs); i++ {
+        pair := &sol.vcPairs[i]
+        pair.uses = 0
+
+        // Count every use
+        for _, reqID := range pair.affectedRequests {
+            req := &p.requestBatches[reqID]
+            cacheUsed := getCacheUsedByRequest(p, sol, req)
+
+            if cacheUsed == pair.cacheID {
+                pair.uses++
+            } else if cacheUsed != -1 {
+				// Memory optimization
+				if pair.ignoreSet == nil {
+					pair.ignoreSet = make(map[int]bool)
+				}
+                pair.ignoreSet[reqID] = true
+            }
+        }
+
+        if pair.assigned {
+            if pair.uses == 0 {
+                //fmt.Printf("Pair is wasteful %+v\n", pair)
+                wastefulPairs++
+                wastefulSet = append(wastefulSet, i)
+            } else {
+                //fmt.Printf("Pair is useful %+v\n", pair)
+                usefulPairs++
+            }
+        }
+    }
+
+    // Remove wasteful pairs from the solution
+    for _, pairIndex := range wastefulSet {
+        pair := &sol.vcPairs[pairIndex]
+        pair.assigned = false
+
+        ass := sol.assignments[pair.cacheID]
+        ass.videos[pair.videoID] = false
+        videoSize := p.videoSizes[pair.videoID]
+        ass.usedCapacity -= videoSize
+    }
+
+    fmt.Printf("Useful pairs %d wasteful pairs %d\n", usefulPairs, wastefulPairs)
+}
+
+func solve(p *Problem) (sol Solution) {
 	fmt.Println("Beginning to solve...")
 
 	setPreferredCaches(p)
-	sortProblemRequests(p)
+    setEndpointRequests(p)
+    //setVideoRequests(p, &sol)
+	//sortProblemRequests(p)
 	initSolutionAssignments(p, &sol)
+    initSolutionVCPairs(p, &sol)
 
-	fmt.Printf("There are %d requests\n", p.numRequestBatches)
+    // Main loop
+    iterations := 5
+    for i:=0; i < iterations; i++ {
+        fmt.Printf("ITERATION %d\n", i)
+        setVCPairSavings(p, &sol)
+        sortSolutionVCPairs(p, &sol)
+        doAssignments(p, &sol)
+        calculateWaste(p, &sol)
+    }
 
+    /*
+    for _, pair := range sol.vcPairs {
+        videoSize := p.videoSizes[pair.videoID]
+        ass := &sol.assignments[pair.cacheID]
+
+		if ass.usedCapacity+videoSize <= p.cacheCapacity {
+			ass.videos[pair.videoID] = true
+			ass.usedCapacity += videoSize
+        }
+    }
+    */
+
+    /*
 	for _, req := range p.requestBatches {
 		var videoSize int = p.videoSizes[req.videoID]
 		var endpoint *Endpoint = &p.endpoints[req.endpointID]
@@ -375,12 +708,6 @@ func solveWithRequestOrder(p *Problem) (sol Solution) {
 
 		for _, idLatPair := range endpoint.preferredCaches {
 			// If there's a substantial saving from a better cache then use it
-			/*
-				if alreadyCached && float64(idLatPair.latency) > 0.1*float64(alreadyCachedLatency) {
-					break
-				}
-			*/
-
 			var ass *CacheAssignment = &sol.assignments[idLatPair.id]
 			if ass.usedCapacity+videoSize <= p.cacheCapacity {
 				//fmt.Printf("Cached in %v\n", ass.cacheID)
@@ -391,8 +718,10 @@ func solveWithRequestOrder(p *Problem) (sol Solution) {
 			}
 		}
 	}
+    */
 
-	fmt.Printf("Finished!")
+    printSolution(&sol)
+	fmt.Printf("Finished! Score %v\n", computeSolutionScore(p, &sol))
 	return sol
 }
 
@@ -411,6 +740,6 @@ func main() {
 	}
 	fmt.Printf("Input file is %v\n", inputPath)
 
-	solution := solveWithRequestOrder(&problem)
+	solution := solve(&problem)
 	writeSolutionFile(inputPath+".solution", &solution)
 }
